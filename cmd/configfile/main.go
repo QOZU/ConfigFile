9aeb2eff38a9bfa3489961e160ee1175c3a0863f
QOZU <64510789+QOZU@users.noreply.github.com>
2025-08-06 03:57:51 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/configfile/main.go
@@ -0,0 +1,23 @@
+// cmd/configfile/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"configfile/internal/configfile"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := configfile.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
